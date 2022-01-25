bda11323e42db1781e110de6e5b07d1d00a1cc3d
Junho <jhshin9345@gmail.com>
2022-01-25 14:43:29 +0900

    solved Q2

--- studylog/algorithm/cpp_codingTest/Q2/code.cpp
+++ studylog/algorithm/cpp_codingTest/Q2/code.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-
 using namespace std;
+int main(int argc, char* argv[]){
+	freopen(argv[1], "rt", stdin);
+	freopen(argv[2], "w", stdout);    
 
-int main(){
-    
+    int a, b, i, sum=0;
+    cin >> a >> b;
+    for(i=a; i<b; i++){
+        cout<<i<<"+";
+        sum=sum+i;
+    }
+    cout<<b<<"=";
+    cout<<sum+b+i;
+    return 0;
 }
