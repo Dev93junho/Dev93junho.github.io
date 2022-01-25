a869bfb0ac90c2df89bc0afe893060b24c4e2611
Junho <jhshin9345@gmail.com>
2022-01-25 14:37:41 +0900

    solved Q1

--- studylog/algorithm/cpp_codingTest/Q1/code.cpp
+++ studylog/algorithm/cpp_codingTest/Q1/code.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
-    
+int main(int argc, char* argv[]){
+	freopen(argv[1], "rt", stdin);
+	freopen(argv[2], "w", stdout);
+    int n, m, i, sum=0;
+    cin >>n>>m;
+    for(i=1; i<=n; i++){
+        if(i%m==0){
+            sum=sum+i;
+        }
+    }
+    cout << sum;
+    return 0;
 }
